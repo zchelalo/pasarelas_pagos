a170d14605f1814037bc08eb73886472b3db37e2
zchelalo <eduardosaavedra687@gmail.com>
2024-01-17 17:12:04 -0700

    fix tipo_usuario.router

--- src/routes/tipos_usuario.router.js
+++ src/routes/tipos_usuario.router.js
@@ -1,53 +1,53 @@
 import express from 'express'
 
-import { Tipos_usuarioService } from '../services/tipos_usuario.service.js'
+import { TiposUsuarioService } from '../services/tipos_usuario.service.js'
 import { validatorHandler } from '../middlewares/validator.handler.js'
-import { updateTipos_usuarioSchema, createTipos_usuarioSchema, getTipos_usuarioSchema } from '../schemas/tipos_usuario.schema.js'
+import { updateTiposUsuarioSchema, createTiposUsuarioSchema, getTiposUsuarioSchema } from '../schemas/tipos_usuario.schema.js'
 
 const router = express.Router()
-const service = new Tipos_usuarioService()
+const service = new TiposUsuarioService()
 
 router.get('/', async (req, res, next) => {
   try {
-    const tipos_usuarios = await service.find()
-    res.json(tipos_usuarios)
+    const tiposUsuarios = await service.find()
+    res.json(tiposUsuarios)
   } catch (error) {
     next(error)
   }
 })
 
-router.get('/:id', validatorHandler(getTipos_usuarioSchema, 'params'), async (req, res, next) => {
+router.get('/:id', validatorHandler(getTiposUsuarioSchema, 'params'), async (req, res, next) => {
   try {
     const { id } = req.params
-    const tipos_usuario = await service.findOne(id)
-    res.json(tipos_usuario)
+    const tiposUsuario = await service.findOne(id)
+    res.json(tiposUsuario)
   } catch (error) {
     next(error)
   }
 })
 
-router.post('/', validatorHandler(createTipos_usuarioSchema, 'body'), async (req, res, next) => {
+router.post('/', validatorHandler(createTiposUsuarioSchema, 'body'), async (req, res, next) => {
   try {
     const body = req.body
-    const newTipos_usuario = await service.create(body)
-    res.status(201).json(newTipos_usuario)
+    const newTiposUsuario = await service.create(body)
+    res.status(201).json(newTiposUsuario)
   } catch (error) {
     next(error)
   }
 })
 
-router.patch('/:id', validatorHandler(getTipos_usuarioSchema, 'params'), validatorHandler(updateTipos_usuarioSchema, 'body'), async (req, res, next) => {
+router.patch('/:id', validatorHandler(getTiposUsuarioSchema, 'params'), validatorHandler(updateTiposUsuarioSchema, 'body'), async (req, res, next) => {
   try {
     const { id } = req.params
     const body = req.body
-    const tipos_usuario = await service.update(id, body)
-    res.json(tipos_usuario)
+    const tiposUsuario = await service.update(id, body)
+    res.json(tiposUsuario)
   } catch (error) {
     next(error)
   }
 })
 
-router.delete('/:id', validatorHandler(getTipos_usuarioSchema, 'params'), async (req, res, next) => {
+router.delete('/:id', validatorHandler(getTiposUsuarioSchema, 'params'), async (req, res, next) => {
   try {
     const { id } = req.params
     await service.delete(id)
